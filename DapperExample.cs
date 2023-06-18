42dcfa0ca6d08c6fd64856ef9fe25793537cbedf
Niraj Ranasinghe <37408192+nirajshiwantha@users.noreply.github.com>
2023-06-18 12:56:26 +0530

    Add files via upload

---
+++ DapperExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DapperExample
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=.; Database=DapperExample; Trusted_Connection=true; TrustServerCertificate=true;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Retrieve all products from the database using Dapper
+                List<Product> products = connection.Query<Product>("SELECT * FROM Products").AsList();
+
+                // Display the retrieved products
+                Console.WriteLine("Products:");
+                foreach (Product product in products)
+                {
+                    Console.WriteLine($"ID: {product.Id}, Name: {product.Name}, Price: {product.Price}, Description: {product.Description}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
